ca482c01434fe7d26f9a51541a186a6fcbcd805c
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-07-02 15:38:04 +0200

    Add some verification and timings

--- run.py
+++ run.py
@@ -13,12 +13,13 @@ import torch
 import cv2
 import maskcompression
 import matplotlib.pyplot as plt
+import time
 
 def compress_mask(mask : torch.Tensor) -> torch.Tensor:
     compressed = torch.unique_consecutive(mask.flatten(), return_counts=True)[1].to(torch.int32)
     return compressed
 
-mask = cv2.imread("C:/Users/zingsheim/Documents/Repositories/VCI_data/Test2/mask_00_15.jpg", 0)
+mask = cv2.imread("C:/Users/zingsheim/Documents/Repositories/VCI_data/Test2/mask_00_15.png", 0)
 
 mask = torch.from_numpy(mask)
 
@@ -28,10 +29,28 @@ compressed = torch.unique_consecutive(mask.flatten(), return_counts=True)[1].to(
 
 print(compressed)
 
-decompressed = maskcompression.decompress(compressed, (mask.shape[0], mask.shape[1]))
+# warmup
+for i in range(10):
+    decompressed = maskcompression.decompress(compressed, (mask.shape[0], mask.shape[1]))
+
+n = 40
+
+start = time.time()
+for i in range(n):
+    decompressed = maskcompression.decompress(compressed, (mask.shape[0], mask.shape[1]))
+end = time.time()
+
+print(f"{(end - start) * 1000.} ms")
+
+decompressed = decompressed.cpu()
 
 plt.imshow(mask)
 plt.show()
 
-plt.imshow(decompressed.cpu())
+plt.imshow(decompressed)
+plt.show()
+
+difference = mask.to(torch.float32) / 255. - decompressed
+
+plt.imshow(difference.cpu())
 plt.show()
