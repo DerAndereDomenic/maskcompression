056a472fa740625fb955a5a017e02bcf6a42ee25
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-07-02 14:51:45 +0200

    Add test compression

--- run.py
+++ run.py
@@ -9,6 +9,20 @@ charonload.module_config["maskcompression"] = charonload.Config(
     stubs_directory=VSCODE_STUBS_DIRECTORY,  # optional
 )
 
+import torch
+import cv2
 import maskcompression
 
-maskcompression.helloworld()
+def compress_mask(mask : torch.Tensor) -> torch.Tensor:
+    compressed = torch.unique_consecutive(mask.flatten(), return_counts=True)[1].to(torch.int32)
+    return compressed
+
+mask = cv2.imread("C:/Users/zingsheim/Documents/Repositories/VCI_data/Test2/mask_00_15.jpg", 0)
+
+mask = torch.from_numpy(mask)
+
+mask[(mask > 0) & (mask < 255)] = 255
+
+compressed = torch.unique_consecutive(mask.flatten(), return_counts=True)[1].to(torch.int32)
+
+print(compressed)
