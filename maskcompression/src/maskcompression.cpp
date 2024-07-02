f9ebb7af09cef75afa4290a738bf531dc3e17355
Domenic Zingsheim <domeniczingsheim@gmail.com>
2024-07-02 14:48:48 +0200

    Add clang format

--- maskcompression/src/maskcompression.cpp
+++ maskcompression/src/maskcompression.cpp
@@ -1,8 +1,6 @@
 #include <torch/python.h>
 
-torch::Tensor generate_tensor();  // Implemented somewhere in <my_cpp_cuda_ext>
-
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-    m.def("helloworld", []{printf("Hello World!");});
+    m.def("helloworld", [] { printf("Hello World!"); });
 }
